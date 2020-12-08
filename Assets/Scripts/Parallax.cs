76fbeafbd5f7c0d1eed3e1541c6b65ac0cf9705f
512233 <512233@chrome.escg.ac.uk>
2020-12-08 11:52:41 +0000

    added menu

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Transform cameraTransform;
+    
+}
---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+
+    public float speed 	= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponent<Renderer>().material.mainTextureOffset 	= new Vector2 (Time.time * speed, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
