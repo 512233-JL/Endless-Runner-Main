fd76d6b0afc4772824db5d3ede4e6c397d0ae58e
512233 <512233@chrome.escg.ac.uk>
2020-12-10 16:11:06 +0000

    made menu

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private Transform cameraTransform;
-    
+    float scrollSpeed = -3f;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 57.5f);
+        transform.position = startPos + Vector2.right * newPos;
+    }
 }
